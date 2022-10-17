1fc57d8f16cc416983161d4a54b472c9173cd969
trunghieu <trunghieu0451@gmail.com>
2022-10-17 20:53:02 +0700

    add fullscreen

--- ghibli-spaces/src/App.js
+++ ghibli-spaces/src/App.js
@@ -6,19 +6,31 @@ import Pomodoro from "./components/Pomodoro/Pomodoro";
 import Todo from "./components/TodoList/Todo";
 import DarkMode from "./components/DarkMode/DarkMode";
 import ColorContext from "./contexts/ColorContext";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 const App = () => {
   const [color, setColor] = useState("#1d2021");
+  const handle = useFullScreenHandle();
 
   return (
     <div className="app">
-      <ColorContext.Provider value={{ color: color, setColor: setColor }}>
-        <Spaces />
-        <Video />
-        <Pomodoro />
-        <Todo />
-        <DarkMode />
-      </ColorContext.Provider>
+      <div>
+        <FullScreen handle={handle}>
+          <ColorContext.Provider value={{ color: color, setColor: setColor }}>
+            <Spaces />
+            <Video />
+            <Pomodoro />
+            <Todo />
+            <DarkMode />
+            <button
+              onClick={handle.enter}
+              style={{ position: "absolute", top: "1px", right: "12px" }}
+            >
+              Enter fullscreen
+            </button>
+          </ColorContext.Provider>
+        </FullScreen>
+      </div>
     </div>
   );
 };
