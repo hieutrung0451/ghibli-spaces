8ffee592c58d0498a1fc2c90572e948749c0e032
trunghieu <trunghieu0451@gmail.com>
2022-10-26 22:10:19 +0700

    add fullscreen exit button

--- ghibli-spaces/src/App.js
+++ ghibli-spaces/src/App.js
@@ -8,7 +8,7 @@ import LanguagesChange from "./components/LanguagesChange/LanguagesChange";
 import Homepage from "./components/Homepage/Homepage";
 import ColorContext from "./contexts/ColorContext";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
-import { BsArrowsFullscreen } from "react-icons/bs";
+import { BsArrowsFullscreen, BsFullscreenExit } from "react-icons/bs";
 import { ImExit } from "react-icons/im";
 import { useTranslation } from "react-i18next";
 import { Routes, Route, NavLink } from "react-router-dom";
@@ -28,21 +28,40 @@ const Main = () => {
   const [color, setColor] = useState("#1d2021");
   const handle = useFullScreenHandle();
   const { t, i18n } = useTranslation();
+  const [fullscreen, setFullsceen] = useState(true);
   return (
     <div>
       <FullScreen handle={handle}>
         <ColorContext.Provider value={{ color: color, setColor: setColor }}>
           <Spaces translation={t} />
           <Video />
-          <Tools translation={t}/>
+          <Tools translation={t} />
           <DarkMode />
-          <button
-            onClick={handle.enter}
-            className="btn-fullscreen"
-            style={{ backgroundColor: color }}
+
+          <div
+            onClick={() => {
+              setFullsceen(!fullscreen);
+            }}
           >
-            <BsArrowsFullscreen />
-          </button>
+            {fullscreen ? (
+              <button
+                onClick={handle.enter}
+                className="btn-fullscreen"
+                style={{ backgroundColor: color }}
+              >
+                <BsArrowsFullscreen />
+              </button>
+            ) : (
+              <button
+                onClick={handle.exit}
+                className="btn-fullscreenExit"
+                style={{ backgroundColor: color }}
+              >
+                <BsFullscreenExit />
+              </button>
+            )}
+          </div>
+
           <button className="btn-exit" style={{ backgroundColor: color }}>
             <NavLink to="/">
               <ImExit />
